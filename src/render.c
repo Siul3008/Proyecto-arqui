d7fd0d913b23e6d96ecd2d28d7e38d8560b99eb4
L.Rivas <luisda.rivas30@gmail.com>
2026-06-01 22:55:56 -0600

    Merge branch 'feature/Siul3008' of https://github.com/Siul3008/Proyecto-arqui into feature/Siul3008 ~ ~ ~ ~ ~ ~ .git/MERGE_MSG [unix] (22:50 01/06/2026)                                                                                                                                      1,1 All"/f/U/Arqui/ProyectoFinal/SummerCarnival92Recca/.git/MERGE_MSG" [unix] 6L, 326B ranch 'feature/Siul3008' of https://github.com/Siul3008/Proyecto-arqui into feature/Siul3008 ~ ~ ~ ~ ~ ~ .git/MERGE_MSG [unix] (22:50 01/06/2026)                                                                                                                                      1,1 All"/f/U/Arqui/ProyectoFinal/SummerCarnival92Recca/.git/MERGE_MSG" [unix] 6L, 326B

--- src/render.c
+++ src/render.c
@@ -36,7 +36,7 @@ static int color_for_char(char value)
 {
     switch (value) {
     case '|':
-    case '*':
+    //case '*':
     case 'O':
     case 'F':
     case 'S':
@@ -55,6 +55,10 @@ static int color_for_char(char value)
         return COLOR_PAIR_ENEMY;
     case 'o':
         return COLOR_PAIR_ENEMY_SHOT;
+    case '*':
+        return COLOR_PAIR_PLAYER_SHOT;
+    case 'A':
+        return COLOR_PAIR_PLAYER;
     default:
         return COLOR_PAIR_TEXT;
     }
@@ -336,21 +340,9 @@ void render_draw(const GameState *game)
         }
     }
 
-    if (game->player.invulnerable_timer == 0 ||
-        (game->player.invulnerable_timer / 5) % 2 == 0) {
-        attron(COLOR_PAIR(COLOR_PAIR_PLAYER));
-        mvprintw(game->player.position.y + 3, game->player.position.x, "/A\\");
-        attroff(COLOR_PAIR(COLOR_PAIR_PLAYER));
-    }
-
-    if (game->player.charge_frames > 0 && game->player.position.y > 0) {
-        int charge_color = game->player.charge_frames >= PLAYER_CHARGE_RELEASE_MIN ?
-                           COLOR_PAIR_PLAYER_SHOT :
-                           COLOR_PAIR_PLAYER;
-        attron(COLOR_PAIR(charge_color));
-        mvaddch(game->player.position.y + 2, game->player.position.x + 1, 'O');
-        attroff(COLOR_PAIR(charge_color));
-    }
+    attron(COLOR_PAIR(COLOR_PAIR_PLAYER));
+    mvprintw(game->player.position.y + 3, game->player.position.x, "/⮝\\");
+    attroff(COLOR_PAIR(COLOR_PAIR_PLAYER));
 
     attron(COLOR_PAIR(COLOR_PAIR_BORDER));
     mvaddch(GAME_HEIGHT + 3, 0, '+');
